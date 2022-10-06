3f950359c8d6a6944dd9e925f87383805aab9030
Wayne <yky32is@gmail.com>
2022-10-07 01:45:14 +0800

    Add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlockchainsModule } from './blockchains/blockchains.module';
 
 @Module({
-  imports: [],
+  imports: [BlockchainsModule],
   controllers: [AppController],
   providers: [AppService],
 })
