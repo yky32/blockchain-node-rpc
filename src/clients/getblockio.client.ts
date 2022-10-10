a25383df4985751345154c8a8b63703618ba0a6d
Wayne Yu <wayneyu.dev@gmail.com>
2022-10-10 12:17:21 +0800

    Add

--- src/clients/getblockio.client.ts
+++ src/clients/getblockio.client.ts
@@ -1,12 +1,32 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class GetBlockIoClient {
   constructor(private readonly httpService: HttpService) {}
 
-  execute(address: string) {
-    const config = {};
-    return this.httpService.get(`https://www.googke.com`, config);
+  async execute(address: string) {
+    const config = {
+      headers: {
+        'x-api-key': '60a12f63-a1c8-446b-8cc6-a94c1a0f62a2',
+      },
+    };
+    const data = {
+      jsonrpc: '2.0',
+      method: 'getblockhash',
+      params: [2346271],
+      id: 'getblock.io',
+    };
+
+    const response = await firstValueFrom(
+      this.httpService.post(`https://ltc.getblock.io/mainnet/`, data, config),
+    );
+
+    if (response) {
+      return response.data;
+    } else {
+      // exception here.
+    }
   }
 }
---
+++ src/utils/axios.ts
@@ -0,0 +1,9 @@
+class axios {
+  get() {
+    return '';
+  }
+
+  post() {
+    return '';
+  }
+}
