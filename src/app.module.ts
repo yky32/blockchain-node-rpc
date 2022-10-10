9c64883e8bf1bb167dccd6b4680dea5da54b1cef
Wayne <yky32is@gmail.com>
2022-10-10 16:32:31 +0800

    Add env based on env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TransactionsModule } from "./transactions/transactions.module";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TransactionsModule } from './transactions/transactions.module';
+import { getEnvPath } from './common/helper/env.helper';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
   imports: [
-    TransactionsModule, 
+    TransactionsModule,
     ConfigModule.forRoot({
+      envFilePath,
       isGlobal: true,
-    })
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-
 @Injectable()
 export class AppService {
-  
-  
   getHello(): string {
-
     return 'Hello World!';
   }
 }
